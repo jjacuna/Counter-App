f0cd11976c775fffda5025671f5046b3068d9565
jjacuna <47874680+jjacuna@users.noreply.github.com>
2019-03-12 22:52:45 -0700

    First commit - button increment implemented

---
+++ src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  //event handler
+  handleIncrement = product => {
+    console.log(product);
+    // method to update state
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
+
+export default Counter;
